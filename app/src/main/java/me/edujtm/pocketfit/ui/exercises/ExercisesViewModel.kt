55fcf55cd587d385fef6244fcb7d260233072132
Eduardo Macedo <eduzemacedo@hotmail.com>
2022-12-17 16:48:54 -0300

    fix: fixes coroutine scope for viewmodels

--- app/src/main/java/me/edujtm/pocketfit/ui/exercises/ExercisesViewModel.kt
+++ app/src/main/java/me/edujtm/pocketfit/ui/exercises/ExercisesViewModel.kt
@@ -3,6 +3,7 @@ package me.edujtm.pocketfit.ui.exercises
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import me.edujtm.pocketfit.infra.persistence.dao.ExerciseDao
 import javax.inject.Inject
@@ -11,18 +12,14 @@ import kotlin.coroutines.CoroutineContext
 class ExercisesViewModel
     @Inject constructor(
         private val exercisesDao: ExerciseDao
-    ) : ViewModel(), CoroutineScope {
-
-    private val job = Job()
-    override val coroutineContext: CoroutineContext
-        get() = job + Dispatchers.Main
+    ) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "Loading..."
     }
     val text: LiveData<String> = _text
 
-    fun fetchExercises() = launch {
+    fun fetchExercises() = viewModelScope.launch {
         val exercises = withContext(Dispatchers.IO) {
             exercisesDao.getExercises()
         }
